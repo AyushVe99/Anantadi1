df5a8e919cf562b0ce06306cc01143e094b620eb
Ayush Verma <112403149+AyushVe99@users.noreply.github.com>
2023-06-12 13:20:56 +0530

    Update App.js

--- src/client/App.js
+++ src/client/App.js
@@ -1,14 +1,89 @@
-import React from 'react';
-import './app.css';
-import LoginForm from './LoginForm';
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import {
+  Container,
+  Grid,
+  Typography,
+  TextField,
+  Button,
+  Paper,
+} from '@material-ui/core';
 
+const useStyles = makeStyles((theme) => ({
+  container: {
+    marginTop: theme.spacing(4),
+  },
+  paper: {
+    padding: theme.spacing(3),
+  },
+  textField: {
+    marginBottom: theme.spacing(2),
+  },
+}));
 
-function App() {
+const App = () => {
+  const classes = useStyles();
+  const [videoFiles, setVideoFiles] = useState([]);
+  const [jsonFiles, setJsonFiles] = useState([]);
+
+  const handleVideoUpload = (event) => {
+    const files = Array.from(event.target.files);
+    setVideoFiles([...videoFiles, ...files]);
+  };
+
+  const handleJsonUpload = (event) => {
+    const files = Array.from(event.target.files);
+    setJsonFiles([...jsonFiles, ...files]);
+  };
+
   return (
-    <div>
-      <LoginForm />
-    </div>
+    <Container className={classes.container} maxWidth="md">
+      <Grid container spacing={3} justify="center">
+        <Grid item xs={12}>
+          <Typography variant="h4" align="center" gutterBottom>
+            Upload Videos and JSON Objects
+          </Typography>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Paper className={classes.paper}>
+            <Typography variant="h6" gutterBottom>
+              Upload Videos
+            </Typography>
+            <input
+              type="file"
+              accept="video/*"
+              multiple
+              onChange={handleVideoUpload}
+            />
+            {videoFiles.map((file, index) => (
+              <Typography key={index}>{file.name}</Typography>
+            ))}
+          </Paper>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Paper className={classes.paper}>
+            <Typography variant="h6" gutterBottom>
+              Upload JSON Objects
+            </Typography>
+            <input
+              type="file"
+              accept=".json"
+              multiple
+              onChange={handleJsonUpload}
+            />
+            {jsonFiles.map((file, index) => (
+              <Typography key={index}>{file.name}</Typography>
+            ))}
+          </Paper>
+        </Grid>
+        <Grid item xs={12}>
+          <Button variant="contained" color="primary">
+            Upload
+          </Button>
+        </Grid>
+      </Grid>
+    </Container>
   );
-}
+};
 
 export default App;
